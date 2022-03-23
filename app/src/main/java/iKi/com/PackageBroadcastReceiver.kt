d2c610e494abfa86c80d82e20e8c371821ddb185
Nhienvtq <nhien.vtq@gmail.com>
2022-03-24 06:40:16 +0700

    update broadcast receiver background cycle

--- app/src/main/java/iKi/com/PackageBroadcastReceiver.kt
+++ app/src/main/java/iKi/com/PackageBroadcastReceiver.kt
@@ -1,12 +1,69 @@
 package iKi.com
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.os.Build
 import android.widget.Toast
 
 class PackageBroadcastReceiver: BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         Toast.makeText(context, "Custom intent received", Toast.LENGTH_SHORT).show()
+        if (context != null) {
+            notifyAlert("hello","received", context)
+        }
+    }
+
+    private lateinit var notificationManager: NotificationManager
+    private lateinit var notificationChannel: NotificationChannel
+    private lateinit var builder: Notification.Builder
+    private fun notifyAlert(title: String, content: String,context: Context){
+        val channelId = context.getString(R.string.channel_name)
+        val description = context.resources.getString(R.string.channel_description)
+        notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val intent = Intent(context, DisplayNotificacionActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(context,0,intent,
+            PendingIntent.FLAG_UPDATE_CURRENT)
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            notificationChannel = NotificationChannel(channelId,description,
+                NotificationManager.IMPORTANCE_HIGH)
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.BLUE
+            notificationChannel.enableVibration(true)
+            notificationManager.createNotificationChannel(notificationChannel)
+
+            builder = Notification.Builder(context, channelId)
+                .setContentTitle(title)
+                .setContentText(content)
+                .setSmallIcon(R.drawable.ic_chat)
+                .setLargeIcon(
+                    BitmapFactory.decodeResource(
+                        context.resources,
+                        R.drawable.ic_home
+                    )
+                )
+                .setContentIntent(pendingIntent)
+        } else {
+            builder = Notification.Builder(context)
+                .setContentTitle(title)
+                .setContentText(content)
+                .setSmallIcon(R.drawable.ic_chat)
+                .setLargeIcon(
+                    BitmapFactory.decodeResource(
+                        context.resources,
+                        R.drawable.ic_home
+                    )
+                )
+                .setContentIntent(pendingIntent)
+        }
+        notificationManager.notify(1234, builder.build())
+
     }
 }
